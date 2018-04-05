68b5b7fac30e5a2aa4b913e3f23c6ed05e5799e7
Yong Tian <tiany@sustc.edu.cn>
2018-04-05 16:04:33 +0800

    update license

--- Heiflow.Controls/Display/Win3DView.cs
+++ Heiflow.Controls/Display/Win3DView.cs
@@ -1,4 +1,33 @@
-﻿using Heiflow.Applications;
+﻿//
+// The Visual HEIFLOW License
+//
+// Copyright (c) 2015-2018 Yong Tian, SUSTech, Shenzhen, China. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// Note: only part of the files distributed in the software belong to the Visual HEIFLOW. 
+// The software also contains contributed files, which may have their own copyright notices.
+//  If not, the GNU General Public License holds for them, too, but so that the author(s) 
+// of the file have the Copyright.
+
+using Heiflow.Applications;
 using Heiflow.Controls.WinForm.Controls;
 using Heiflow.Core.Data;
 using Heiflow.Presentation.Controls;
--- Heiflow.Core/Data/Excel/ExcelBinaryReader.cs
+++ Heiflow.Core/Data/Excel/ExcelBinaryReader.cs
@@ -1,8 +1,32 @@
-// THIS FILE IS PART OF Visual HEIFLOW
-// THIS PROGRAM IS NOT FREE SOFTWARE. 
-// Copyright (c) 2015-2017 Yong Tian, SUSTech, Shenzhen, China. All rights reserved.
-// Email: tiany@sustc.edu.cn
-// Web: http://ese.sustc.edu.cn/homepage/index.aspx?lid=100000005794726
+//
+// The Visual HEIFLOW License
+//
+// Copyright (c) 2015-2018 Yong Tian, SUSTech, Shenzhen, China. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// Note: only part of the files distributed in the software belong to the Visual HEIFLOW. 
+// The software also contains contributed files, which may have their own copyright notices.
+//  If not, the GNU General Public License holds for them, too, but so that the author(s) 
+// of the file have the Copyright.
+
 using System;
 using System.Collections.Generic;
 using System.Data;
