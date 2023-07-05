5fab3282be53b65494ee3e2c4c34ec91728f2b5a
Yong Tian <tiany@sustech.edu.cn>
2023-07-05 17:57:16 +0800

    add mt3d pck

--- src/Heiflow.Controls/Project/CreatHeiflowFromMFForm.Designer.cs
+++ src/Heiflow.Controls/Project/CreatHeiflowFromMFForm.Designer.cs
@@ -48,6 +48,9 @@
             this.tbBTN = new System.Windows.Forms.TextBox();
             this.btnBtnpck = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbInitConcFile = new System.Windows.Forms.TextBox();
+            this.btnOpenInitConc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -55,7 +58,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.Size = new System.Drawing.Size(175, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "Modflow BAS File";
             // 
@@ -65,14 +68,14 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbBASfile.Location = new System.Drawing.Point(171, 7);
             this.tbBASfile.Name = "tbBASfile";
-            this.tbBASfile.Size = new System.Drawing.Size(485, 27);
+            this.tbBASfile.Size = new System.Drawing.Size(659, 35);
             this.tbBASfile.TabIndex = 1;
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(670, 207);
+            this.btnCancel.Location = new System.Drawing.Point(844, 266);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(100, 30);
             this.btnCancel.TabIndex = 4;
@@ -83,7 +86,7 @@
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(556, 207);
+            this.btnOK.Location = new System.Drawing.Point(730, 266);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(100, 30);
             this.btnOK.TabIndex = 5;
@@ -94,7 +97,7 @@
             // btnBrowseBAS
             // 
             this.btnBrowseBAS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowseBAS.Location = new System.Drawing.Point(670, 4);
+            this.btnBrowseBAS.Location = new System.Drawing.Point(844, 4);
             this.btnBrowseBAS.Name = "btnBrowseBAS";
             this.btnBrowseBAS.Size = new System.Drawing.Size(100, 30);
             this.btnBrowseBAS.TabIndex = 6;
@@ -107,7 +110,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(14, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.Size = new System.Drawing.Size(106, 30);
             this.label2.TabIndex = 0;
             this.label2.Text = "Projection";
             // 
@@ -117,13 +120,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbPrjName.Location = new System.Drawing.Point(171, 79);
             this.tbPrjName.Name = "tbPrjName";
-            this.tbPrjName.Size = new System.Drawing.Size(485, 27);
+            this.tbPrjName.Size = new System.Drawing.Size(659, 35);
             this.tbPrjName.TabIndex = 1;
             // 
             // btnSelecProjection
             // 
             this.btnSelecProjection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelecProjection.Location = new System.Drawing.Point(670, 77);
+            this.btnSelecProjection.Location = new System.Drawing.Point(844, 77);
             this.btnSelecProjection.Name = "btnSelecProjection";
             this.btnSelecProjection.Size = new System.Drawing.Size(100, 30);
             this.btnSelecProjection.TabIndex = 6;
@@ -135,7 +138,7 @@
             // 
             this.tbX.Location = new System.Drawing.Point(188, 122);
             this.tbX.Name = "tbX";
-            this.tbX.Size = new System.Drawing.Size(182, 27);
+            this.tbX.Size = new System.Drawing.Size(182, 35);
             this.tbX.TabIndex = 1;
             this.tbX.Text = "0";
             // 
@@ -144,7 +147,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(14, 124);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.Size = new System.Drawing.Size(210, 30);
             this.label3.TabIndex = 0;
             this.label3.Text = "Upper left coordinate";
             // 
@@ -154,7 +157,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbY.Location = new System.Drawing.Point(403, 122);
             this.tbY.Name = "tbY";
-            this.tbY.Size = new System.Drawing.Size(253, 27);
+            this.tbY.Size = new System.Drawing.Size(427, 35);
             this.tbY.TabIndex = 1;
             this.tbY.Text = "0";
             // 
@@ -163,7 +166,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(166, 125);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(21, 20);
+            this.label4.Size = new System.Drawing.Size(30, 30);
             this.label4.TabIndex = 0;
             this.label4.Text = "X:";
             // 
@@ -172,7 +175,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(376, 125);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 20);
+            this.label5.Size = new System.Drawing.Size(30, 30);
             this.label5.TabIndex = 0;
             this.label5.Text = "Y:";
             // 
@@ -181,7 +184,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(14, 48);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(123, 20);
+            this.label6.Size = new System.Drawing.Size(170, 30);
             this.label6.TabIndex = 0;
             this.label6.Text = "Modflow DIS File";
             // 
@@ -191,13 +194,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbDIS.Location = new System.Drawing.Point(171, 43);
             this.tbDIS.Name = "tbDIS";
-            this.tbDIS.Size = new System.Drawing.Size(485, 27);
+            this.tbDIS.Size = new System.Drawing.Size(659, 35);
             this.tbDIS.TabIndex = 1;
             // 
             // btnBrowseDIS
             // 
             this.btnBrowseDIS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowseDIS.Location = new System.Drawing.Point(670, 38);
+            this.btnBrowseDIS.Location = new System.Drawing.Point(844, 38);
             this.btnBrowseDIS.Name = "btnBrowseDIS";
             this.btnBrowseDIS.Size = new System.Drawing.Size(100, 30);
             this.btnBrowseDIS.TabIndex = 6;
@@ -210,9 +213,9 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(14, 165);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(127, 20);
+            this.label7.Size = new System.Drawing.Size(150, 30);
             this.label7.TabIndex = 0;
-            this.label7.Text = "Modflow BAS File";
+            this.label7.Text = "MT3D BTN File";
             // 
             // tbBTN
             // 
@@ -220,14 +223,14 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbBTN.Location = new System.Drawing.Point(170, 165);
             this.tbBTN.Name = "tbBTN";
-            this.tbBTN.Size = new System.Drawing.Size(485, 27);
+            this.tbBTN.Size = new System.Drawing.Size(659, 35);
             this.tbBTN.TabIndex = 1;
             this.tbBTN.Text = "E:\\mf.btn";
             // 
             // btnBtnpck
             // 
             this.btnBtnpck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBtnpck.Location = new System.Drawing.Point(670, 160);
+            this.btnBtnpck.Location = new System.Drawing.Point(844, 160);
             this.btnBtnpck.Name = "btnBtnpck";
             this.btnBtnpck.Size = new System.Drawing.Size(100, 30);
             this.btnBtnpck.TabIndex = 6;
@@ -239,7 +242,7 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(434, 207);
+            this.button1.Location = new System.Drawing.Point(608, 266);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 30);
             this.button1.TabIndex = 5;
@@ -247,20 +250,52 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnMT3D_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 30);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "MT3D Init File";
+            // 
+            // tbInitConcFile
+            // 
+            this.tbInitConcFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInitConcFile.Location = new System.Drawing.Point(171, 206);
+            this.tbInitConcFile.Name = "tbInitConcFile";
+            this.tbInitConcFile.Size = new System.Drawing.Size(659, 35);
+            this.tbInitConcFile.TabIndex = 1;
+            this.tbInitConcFile.Text = "E:\\init_conc.csv";
+            // 
+            // btnOpenInitConc
+            // 
+            this.btnOpenInitConc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenInitConc.Location = new System.Drawing.Point(844, 206);
+            this.btnOpenInitConc.Name = "btnOpenInitConc";
+            this.btnOpenInitConc.Size = new System.Drawing.Size(100, 30);
+            this.btnOpenInitConc.TabIndex = 6;
+            this.btnOpenInitConc.Text = "Browse...";
+            this.btnOpenInitConc.UseVisualStyleBackColor = true;
+            this.btnOpenInitConc.Click += new System.EventHandler(this.btnOpenInitConc_Click);
+            // 
             // CreatHeiflowFromMFForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 251);
+            this.ClientSize = new System.Drawing.Size(956, 310);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.btnSelecProjection);
             this.Controls.Add(this.btnBrowseDIS);
+            this.Controls.Add(this.btnOpenInitConc);
             this.Controls.Add(this.btnBtnpck);
             this.Controls.Add(this.btnBrowseBAS);
             this.Controls.Add(this.tbY);
             this.Controls.Add(this.tbX);
+            this.Controls.Add(this.tbInitConcFile);
             this.Controls.Add(this.tbBTN);
             this.Controls.Add(this.tbPrjName);
             this.Controls.Add(this.tbDIS);
@@ -269,6 +304,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label1);
@@ -305,5 +341,8 @@
         private System.Windows.Forms.TextBox tbBTN;
         private System.Windows.Forms.Button btnBtnpck;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbInitConcFile;
+        private System.Windows.Forms.Button btnOpenInitConc;
     }
 }
---
+++ src/Heiflow.Tools/ConceptualModel/UZF/UZF2RCHEVT.cs
@@ -0,0 +1,133 @@
+﻿//
+// The Visual HEIFLOW License
+//
+// Copyright (c) 2015-2018 Yong Tian, SUSTech, Shenzhen, China. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// Note:  The software also contains contributed files, which may have their own 
+// copyright notices. If not, the GNU General Public License holds for them, too, 
+// but so that the author(s) of the file have the Copyright.
+//
+
+using DotSpatial.Data;
+using GeoAPI.Geometries;
+using Heiflow.Applications;
+using Heiflow.Controls.WinForm.Editors;
+using Heiflow.Core.Data;
+using Heiflow.Models.Generic;
+using Heiflow.Models.Integration;
+using Heiflow.Models.Subsurface;
+using Heiflow.Models.Tools;
+using Heiflow.Presentation.Services;
+using Heiflow.Spatial.SpatialRelation;
+// THIS FILE IS PART OF Visual HEIFLOW
+// THIS PROGRAM IS NOT FREE SOFTWARE. 
+// Copyright (c) 2015-2017 Yong Tian, SUSTech, Shenzhen, China. All rights reserved.
+// Email: tiany@sustc.edu.cn
+// Web: http://ese.sustc.edu.cn/homepage/index.aspx?lid=100000005794726
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heiflow.Tools.ConceptualModel
+{
+    public class UZF2RCHEVT : MapLayerRequiredTool
+    {
+
+        public UZF2RCHEVT()
+        {
+            Name = "Set IRUNBND Values";
+            Category = Cat_CMG;
+            SubCategory = "UZF";
+            Description = "IRUNBND define the stream segments within the Streamflow-Routing (SFR2) Package or lake numbers in the Lake (LAK3)" +
+           " Package to which overland runoff from excess infiltration and ground-water discharge to land surface will be added.";
+            Version = "1.0.0.0";
+            this.Author = "Yong Tian";
+            MultiThreadRequired = true;
+        }
+        [Category("Output")]
+        [Description("The difference data cube")]
+        public string EVTDataCube
+        {
+            get;
+            set;
+        }
+        [Category("Output")]
+        [Description("The difference data cube")]
+        public string RCHDataCube
+        {
+            get;
+            set;
+        }
+
+        public override void Initialize()
+        {
+     
+        
+        }
+
+        public override bool Execute(ICancelProgressHandler cancelProgressHandler)
+        {
+            var shell = MyAppManager.Instance.CompositionContainer.GetExportedValue<IShellService>();
+            var prj = MyAppManager.Instance.CompositionContainer.GetExportedValue<IProjectService>();
+            var model = prj.Project.Model;
+            int progress = 0;
+            int count = 1;
+            Modflow mf = null;
+            if (model is HeiflowModel)
+                mf = (model as HeiflowModel).ModflowModel;
+            else if (model is Modflow)
+                mf = model as Modflow;
+            if (mf != null)
+            {
+                var pck = mf.GetPackage(UZFPackage.PackageName) as UZFPackage;
+                var mfgrid = mf.Grid as RegularGrid;
+                //Coordinate centroid = null;
+                for (int i = 0; i < mfgrid.ActiveCellCount; i++)
+                {
+                 
+                    progress = i * 100 / mfgrid.ActiveCellCount;
+                    if (progress > count)
+                    {
+                        cancelProgressHandler.Progress("Package_Tool", progress, "Processing cell: " + i);
+                        count++;
+                    }
+                }
+                pck.Save(cancelProgressHandler);
+                return true;
+            }
+            else
+            {
+                cancelProgressHandler.Progress("Package_Tool", 100, "Failed to run.");
+                return false;
+            }
+        }
+    }
+}
+https://shangcheng.biddingoffice.sustech.edu.cn/newmall/newGoodDisplay.do?method=view&id=4814706
+
+https://shangcheng.biddingoffice.sustech.edu.cn/newmall/newGoodDisplay.do?method=view&id=4777994
+
+https://shangcheng.biddingoffice.sustech.edu.cn/newmall/newGoodDisplay.do?method=view&id=4814139
